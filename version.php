24c4a16a1a3b1c18cd6ba116dfe7833f60f9172d
Leconte <marc.leconte@univ-lemans.fr>
2019-04-01 11:10:19 +0200

    Update version.php

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release   = 'v1.1.1';             // The current plugin release
-$plugin->version   = 2019030202;         // The current plugin version (Date: YYYYMMDDXX).10
+$plugin->release   = 'v1.2.0';             // The current plugin release
+$plugin->version   = 2019030825;         // The current plugin version (Date: YYYYMMDDXX).10
 $plugin->requires  = 2012112900;         // Requires this Moodle version.
 $plugin->component = 'tool_attestoodle'; // Full name of the plugin (used for diagnostics).
