97e965861f8d97539b298b690282c139b99ed54b
Didier CADIOU <didier.cadiou@unicaen.fr>
2019-05-31 11:26:39 +0200

    Update version.php

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->release   = 'v1.4.4';             // The current plugin release
-$plugin->version   = 2019043001;         // The current plugin version (Date: YYYYMMDDXX).10
+$plugin->version   = 2019053101;         // The current plugin version (Date: YYYYMMDDXX).10
 $plugin->requires  = 2012112900;         // Requires this Moodle version.
 $plugin->component = 'tool_attestoodle'; // Full name of the plugin (used for diagnostics).
