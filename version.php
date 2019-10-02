b5078d978dded37e3604557b1f5ae9c7a6a9b2ae
Leconte <marc.leconte@univ-lemans.fr>
2019-10-02 09:23:40 +0200

    nom formation unique

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release   = 'v1.7.7';             // The current plugin release
-$plugin->version   = 2019100103;         // The current plugin version (Date: YYYYMMDDXX).10
+$plugin->release   = 'v1.7.8';             // The current plugin release
+$plugin->version   = 2019100203;         // The current plugin version (Date: YYYYMMDDXX).10
 $plugin->requires  = 2012112900;         // Requires this Moodle version.
 $plugin->component = 'tool_attestoodle'; // Full name of the plugin (used for diagnostics).
